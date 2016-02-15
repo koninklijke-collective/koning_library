785ba0a9365147767b70c350b3c2ae1af4386e96
Benjamin Serfhos <serfhos@rsm.nl>
2016-02-15 15:17:24 +0100

    [TASK] Initial empty release

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Koning: Generic Library',
+    'description' => 'ViewHelpers, Abstract classes, Wizards and Utilities',
+    'category' => 'misc',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'author' => 'Jesper Paardekooper,Benjamin Serfhos',
+    'author_email' => 'j.paardekooper@grandslam-media.nl,serfhos@rsm.nl',
+    'author_company' => 'GrandSlamMediaNL',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-7.99.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'Keizer\\KoningLibrary\\' => 'Classes'
+        )
+    ),
+);
