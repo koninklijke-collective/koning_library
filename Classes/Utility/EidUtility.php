b4195b873ec337c3906fbc9125d419c5c270463e
Jesper Paardekooper <j.paardekooper@grandslam-media.nl>
2017-05-10 11:42:26 +0200

    [BUGFIX] Properly simulate a TSFE

--- Classes/Utility/EidUtility.php
+++ Classes/Utility/EidUtility.php
@@ -10,86 +10,33 @@ class EidUtility
 {
 
     /**
-     * Initialize full GLOBALS[TSFE] based on given page id
+     * Initializes TypoScript frontend controller
      *
      * @param integer $pageId
      * @return void
      */
-    public static function initializeFrontendPage($pageId = 0)
+    public static function initializeTypoScriptFrontendController($pageId = 0)
     {
-        static::initializeTypoScriptFrontendController($pageId);
-        static::initializeFrontendUserAuthentication();
+        if (!($GLOBALS['TSFE'] instanceof \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController)) {
+            \TYPO3\CMS\Frontend\Utility\EidUtility::initTCA();
 
-        $controller = &$GLOBALS['TSFE'];
-        if (!($controller->sys_page instanceof \TYPO3\CMS\Frontend\Page\PageRepository)) {
-            $controller->determineId();
-        }
+            if (!is_object($GLOBALS['TT'])) {
+                $GLOBALS['TT'] = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\TimeTracker\NullTimeTracker::class);
+                $GLOBALS['TT']->start();
+            }
 
-        if (!($controller->tmpl instanceof \TYPO3\CMS\Core\TypoScript\TemplateService)) {
-            $controller->initTemplate();
-        }
-
-        if (!is_array($controller->config)) {
-            $controller->getConfigArray();
-        }
-
-        static::initializeContentObjectRenderer();
-
-        if (empty($controller->indexedDocTitle)) {
-            \TYPO3\CMS\Frontend\Page\PageGenerator::pagegenInit();
-        }
-    }
-
-    /**
-     * Initialize TSFE based on given page id
-     *
-     * @param integer $pageId
-     * @return void
-     */
-    public static function initializeTypoScriptFrontendController($pageId = 1)
-    {
-        global $TYPO3_CONF_VARS;
-
-        // fallback for timetracker
-        if (!is_object($GLOBALS['TT'])) {
-            $GLOBALS['TT'] = new \TYPO3\CMS\Core\TimeTracker\NullTimeTracker();
-        }
-
-        $controller = &$GLOBALS['TSFE'];
-        if (!($controller instanceof \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController)) {
-            $controller = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-                'TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController',
-                $TYPO3_CONF_VARS,
-                $pageId,
-                0
-            );
-        }
-    }
-
-    /**
-     * Initialize GLOBALS Frontend User
-     *
-     * @return void
-     */
-    public static function initializeFrontendUserAuthentication()
-    {
-        static::initializeTypoScriptFrontendController();
-        if (!($GLOBALS['TSFE']->fe_user instanceof \TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication)) {
+            $GLOBALS['TSFE'] = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController::class, $GLOBALS['TYPO3_CONF_VARS'], $pageId, 0);
+            $GLOBALS['TSFE']->connectToDB();
             $GLOBALS['TSFE']->initFEuser();
+            $GLOBALS['TSFE']->determineId();
+            $GLOBALS['TSFE']->initTemplate();
+            $GLOBALS['TSFE']->getConfigArray();
+
+            if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
+                $rootLine = \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($pageId);
+                $host = \TYPO3\CMS\Backend\Utility\BackendUtility::firstDomainRecord($rootLine);
+                $_SERVER['HTTP_HOST'] = $host;
+            }
         }
     }
-
-    /**
-     * Initialize GLOBALS Content Object Renderer
-     *
-     * @return void
-     */
-    public static function initializeContentObjectRenderer()
-    {
-        static::initializeTypoScriptFrontendController();
-        if (!($GLOBALS['TSFE']->cObj instanceof \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer)) {
-            $GLOBALS['TSFE']->newCObj();
-        }
-    }
-
 }
