678cfc603b79c58363fa6bf6b1f984124215012e
Benjamin Serfhos <serfhos@rsm.nl>
2023-03-14 11:26:50 +0100

    [TASK] Make sure extension is usable for TYPO3 12

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
 
-defined('TYPO3_MODE') or die('Access denied.');
-
 // Custom Routing Aspects Mapper
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['SimpleValueMapper'] ??=
     \Keizer\KoningLibrary\Routing\Aspect\SimpleValueMapper::class;
